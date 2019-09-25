dfc33cf8bb4425611cd8965b9efcd1252f842471
Andres Medina <amedina@cobalt05.icecube.wisc.edu>
2019-09-25 13:36:05 -0500

    Update

--- Histogram.py
+++ Histogram.py
@@ -3,3 +3,14 @@ import numpy as np
 import matplotlib.pylab as plt
 import argparse
 
+parser = argparse.ArgumentParser(description='Process DNN')
+
+parser.add_argument('-i',
+                    dest = 'input_file',
+                    help='This is the input root file')
+
+parser.add_arguments('-o',
+                     dest = 'output_base',
+                     help='Base of output files for histograms')
+
+args = parser.parse_args()
