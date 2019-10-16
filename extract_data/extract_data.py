abdc79282fd31e787fe71d07deb8db432fb499ca
andyPhysics <medina.101@osu.edu>
2019-10-16 14:19:27 -0400

    example root file

--- extract_data/extract_data.py
+++ extract_data/extract_data.py
@@ -41,101 +41,87 @@ def get_Xmax(depth,num):
     max_value = -x[1]/(2*x[0])
     return max_value
 
-def read_files(filename_list):
+def read_xmax_from_i3_file(event_file_name):
+    print("reading file: {}".format(event_file_name))
+    event_file = dataio.I3File(event_file_name)
+    numEMinus = []
+    numEPlus = []
+    run = []
+    event = []
+    xmax = []
 
-    output_features_IC = []
-    output_labels = []
+    while event_file.more():
+        frame = event_file.pop_physics()
+        long_profile = frame['MCPrimaryInfo'].longProfile
+        event_numEMinus = []
+        event_numEPlus = []
+        event_depth = []
 
-    for event_file_name in filename_list:
-        print("reading file: {}".format(event_file_name))
-        event_file = dataio.I3File(event_file_name)
-        
-        event_number = 0
+        for i in long_profile:
+            event_numEMinus.append(i.numEMinus)
+            event_numEPlus.append(i.numEPlus)
+            event_depth.append(i.depth)
+        numEMinus.append(event_numEMinus)
+        numEPlus.append(event_numEPlus)
+        sum_values = np.array(event_numEMinus)+np.array(event_numEPlus)
+        xmax.append(get_Xmax(event_depth,sum_values))
+        run.append(frame['I3EventHeader'].run_id)
+        event.append(frame['I3EventHeader'].event_id)
+    return zip(xmax,run,event)
 
-        numEMinus = []
-        numEPlus = []
-        depth = []
-        mass = []
-        energy = []
-        xmax = []
-        run = []
-        event = []
-        s125 = []
-        beta = []
-        zenith = []
-        azimuth = []
-        x = []
-        y = []
-        z = []
-        eloss_1500 = []
-        eloss_1800 = []
-        eloss_2100 = []
-        eloss_2400 = []
-        stoch_energy = []
-        rel_stoch_energy = []
-        chi2 = []
-        chi2_red = []
-        stoch_depth = []
-        n_he_stoch = []
-        fit_status = []
-        time_start_mjd_sec = []
-        time_start_mjd_ns = []
-        time_start_mjd_day = []
-        eloss_1500_red = []
-        stoch_energy2 = []
-        rel_stoch_energy2 = []
-        chi2_red2 = []
-        stoch_depth2 = []
-        n_he_stoch2 = []
-        fit_status2 = []
-        eloss_1500_red2 = []
-        mc_weight = []
-        nch = []
-        qtot = []
-        while event_file.more():
-            frame = event_file.pop_physics()
-            long_profile = frame['MCPrimaryInfo'].longProfile
-            event_numEMinus = []
-            event_numEPlus = []
-            event_depth = []
-            for i in long_profile:
-                event_numEMinus.append(i.numEMinus)
-                event_numEPlus.append(i.numEPlus)
-                event_depth.append(i.depth)
-            numEMinus.append(event_numEMinus)
-            numEPlus.append(event_numEPlus)
-            sum_values = np.array(event_numEMinus)+np.array(event_numEPlus)
-            Xmax = xmax.append(get_Xmax(event_depth,sum_values))
-            depth.append(event_depth)
-            mass.append(args.mass)
-            energy.append(frame['MCPrimary'].energy)
-            run.append(frame['I3EventHeader'].run_id)
-            event.append(frame['I3EventHeader'].event_id)
-            s125.append(frame['LaputopSmallParams'].value(Par.Log10_S125))
-            beta.append(frame['LaputopSmallParams'].value(Par.Beta))
-            zenith.append(frame['MCPrimary'].dir.zenith)
-            azimuth.append(frame['MCPrimary'].dir.azimuth)
-            x.append(frame['MCPrimary'].pos.x)
-            y.append(frame['MCPrimary'].pos.y)
-            z.append(frame['MCPrimary'].pos.z)
-            fit_status.append(frame['LaputopSmall'].fit_status)
-            time_start_mjd_sec.append(frame['I3EventHeader'].start_time)
-            print(str(frame['I3EventHeader'].start_time).split()[1].replace(',',''))
-
-#            print(frame['IsSmallShower'].value)
-        print(xmax)
-        my_dict = dict(numEMinus=numEMinus,
-                       numEPlus=numEPlus,
-                       depth=depth,
-                       mass=mass,
-                       energy=energy,
-                       xmax=xmax)
-        # close the input file once we are done
-        del event_file
+def read_root_files(filename):
+    x = uproot.open(filename)
+    run = x['I3EventHeader']['Run'].array()
+    event = x['I3EventHeader']['Event'].array()
+    mass = [] #This is known because it is
+    energy = x['MCPrimary']['energy'].array()
+    depth = x['MCPrimaryInfo']['longDepth'].array()
+    numEPlus = x['MCPrimaryInfo']['longNumEMinus'].array()
+    numEMinus = x['MCPrimaryInfo']['longNumEPlus'].array()
+    xmax = []
+    s70 = x['LaputopParams']['s70'].array()
+    s150 = x['LaputopParams']['s150'].array()
+    s125 = x['LaputopParams']['s125'].array()
+    beta = x['LaputopParams']['beta'].array()
+    zenith = x['MCPrimary']['zenith'].array()
+    azimuth = x['MCPrimary']['azimuth'].array()
+    x = x['MCPrimary']['x'].array()
+    y = x['MCPrimary']['y'].array()
+    z = x['MCPrimary']['z'].array()
+    itsiz = []
+    iisiz = []
+    eloss_1500 = []
+    eloss_1800 = []
+    eloss_2100 = []
+    eloss_2400 = []
+    stoch_energy = []
+    rel_stoch_energy = []
+    chi2 = []
+    chi2_red = []
+    stoch_depth = []
+    n_he_stoch = []
+    fit_status = []
+    time_start_mjd_sec = []
+    time_start_mjd_ns = []
+    time_start_mjd_day = []
+    eloss_1500_red = []
+    stoch_energy2 = []
+    rel_stoch_energy2 = []
+    chi2_red2 = []
+    stoch_depth2 = []
+    n_he_stoch2 = []
+    fit_status2 = []
+    eloss_1500_red2 = []
+    mc_weight = []
+    nch = []
+    qtot = []
+  
+    my_dict = dict(mass=mass,
+                   energy=energy,
+                   xmax=xmax)
     return my_dict
-my_files = ['/data/ana/CosmicRay/IceTop_level3/sim/IC86.2012/12360/Level3_IC86.2012_12360_Run006667.i3.gz']
+my_files = '/data/ana/CosmicRay/IceTop_level3/sim/IC86.2012/12360/Level3_IC86.2012_12360_Run006667.i3.gz'
+
+print(read_xmax_from_i3_file(my_files))
 
 
-values = read_files(my_files)
-
-np.save('First.npy',values)
