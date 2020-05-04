58af8d4a0b73962584d42af65f764787b029da37
andyPhysics <medina.101@osu.edu>
2020-05-04 15:10:18 -0400

    updates

--- get_event.py
+++ get_event.py
@@ -48,8 +48,9 @@ while l3_file.more():
         eventinfo[omkey]['m'] = frame['WaveformInfo'][omkey]['m']
         eventinfo[omkey]['s'] = frame['WaveformInfo'][omkey]['s']
         eventinfo[omkey]['t0'] = frame['WaveformInfo'][omkey]['t_0']
-        eventinfo[omkey]['charge'] = frame['WaveformInfo'][omkey]['Charge_PE']
+        eventinfo[omkey]['charge'] = frame['WaveformInfo'][omkey]['Charge']
         eventinfo[omkey]['chargePe'] = frame['WaveformInfo'][omkey]['Charge_PE']
+        eventinfo[omkey]['chargeVEM'] = frame['LaputopHLCPE'][OMKey(string,dom)][0].charge
         eventinfo[omkey]['chi2'] = frame['WaveformInfo'][omkey]['chi2']
         eventinfo[omkey]['sigmat0'] = frame['WaveformInfo'][omkey]['sigma_t']
         eventinfo[omkey]['sigmam'] = frame['WaveformInfo'][omkey]['sigma_s']
