0abfb10d4898ac980ddcbac9f8454c2003313f6c
Medina, Andres <Amedina2639@gmail.com>
2023-11-22 16:26:57 -0500

    Improvements to code to ensure readability

--- NN/scripts/methods.py
+++ NN/scripts/methods.py
@@ -8,8 +8,24 @@ def weight_function(N,a,b):
     y = a * N + b
     return y
 
-def new_df(df,mass):
-    df['mass'] = [mass for i in range(len(df))]
+def new_df(df, mass):
+    add_basic_features(df, mass)
+    add_derived_features(df)
+    df = apply_energy_cut(df)
+    fit = fit_waveform_weight(df)
+    update_waveform_weight(df, fit)
+    df = apply_additional_filters(df)
+    
+    return df
+
+def new_df_data(df):
+    add_data_features(df)
+    df = apply_data_filters(df)
+    
+    return df
+
+def add_basic_features(df, mass):
+    df['mass'] = mass
     df['log_energy'] = np.log10(df['energy'])
     df['cos_zenith'] = np.cos(df['zenith'])
     df['log_muon_total'] = np.log10(df['muon_total'])
@@ -18,161 +34,88 @@ def new_df(df,mass):
     df['log_s_r'] = np.log10(df['s_r'])
     df['log_charge'] = np.log10(df['charge'])
     df['log_A'] = np.log10(df['A'])
-    m_o = df['m_o'].values
-    m_125 = df['m_125'].values
-    new = [(i-j) for i,j in zip(df['firstint'],df['Xo'])]
-    df['new'] = new
-    df['new_s125'] = [i + 125*j for i,j in zip(df['s_o'],df['s_r'])]
-    df['delta_L'] = df['Xmax'] - df['firstint']
-    energy_cut = []
-    for i in df['log_energy']:
-        if i > 7.0:
-            energy_cut.append(True)
-            continue
-        elif np.random.random() < 0.5:
-            energy_cut.append(True)
-            continue
-        energy_cut.append(False)
-        
-    fit = curve_fit(weight_function,df['N'][(df['waveform_weight']!=0)&(df['N']<75)],np.log10(df['waveform_weight'][(df['waveform_weight']!=0)&(df['N']<75)]))
-    
-    new_waveform_weight = []
-    for i in range(len(df['waveform_weight'])):
-        if (df['waveform_weight'].values[i] == 0)&(df['N'].values[i]<100):
-            new_waveform_weight.append(10**weight_function(df['N'].values[i],fit[0][0],fit[0][1]))
-            continue
-        elif (df['waveform_weight'].values[i] == 0)&(df['N'].values[i]>=100):
-            new_waveform_weight.append(1)
-            continue
-        new_waveform_weight.append(df['waveform_weight'].values[i])
-    df['new_waveform_weight'] = new_waveform_weight
-    
-    df = df.loc[(df['Xmax']<900)&
-                (df['Xmax']>400)&
-                (np.array(energy_cut))&
-                (df['ghRedChiSqr']<400)&
-                (abs(df['max_check'])<20)&
-                (df['S125']>0)&
-                (df['fit_status_m']!=0)&
-                (df['fit_status_m']!=2)&
-                (df['m_chi2']>1e-4)&
-                (df['log_m_r']>-5)&
-                (df['log_m_r']<-1)&
-                (df['m_125']<6)&
-                (df['A']>1e-4)&
-               (df['zenith']<45*(np.pi/180))]
-    return df
 
-def new_df_data(df):
+def add_derived_features(df):
+    df['new'] = df['firstint'] - df['Xo']
+    df['new_s125'] = df['s_o'] + 125 * df['s_r']
+    df['delta_L'] = df['Xmax'] - df['firstint']
+
+def apply_energy_cut(df):
+    energy_cut_condition = (df['log_energy'] > 7.0) | (np.random.random(len(df)) < 0.5)
+    return df[energy_cut_condition]
+
+def fit_waveform_weight(df):
+    fit = curve_fit(weight_function, df['N'][(df['waveform_weight'] != 0) & (df['N'] < 75)],
+                    np.log10(df['waveform_weight'][(df['waveform_weight'] != 0) & (df['N'] < 75)]))
+    return fit
+
+def update_waveform_weight(df, fit):
+    new_waveform_weight_condition = (df['waveform_weight'] == 0) & (df['N'] < 100)
+    df.loc[new_waveform_weight_condition, 'new_waveform_weight'] = 10 ** weight_function(
+        df.loc[new_waveform_weight_condition, 'N'], fit[0][0], fit[0][1])
+    
+    df.loc[(df['waveform_weight'] == 0) & (df['N'] >= 100), 'new_waveform_weight'] = 1
+
+def apply_additional_filters(df):
+    return df.loc[(df['Xmax'] < 900) & (df['Xmax'] > 400) &
+                (df['ghRedChiSqr'] < 400) & (abs(df['max_check']) < 20) &
+                (df['S125'] > 0) & (df['fit_status_m'].isin([0, 2])) &
+                (df['m_chi2'] > 1e-4) & (-5 < df['log_m_r'] < -1) &
+                (df['m_125'] < 6) & (df['A'] > 1e-4) &
+                (df['zenith'] < 45 * (np.pi / 180))]
+
+def add_data_features(df):
     df['cos_zenith'] = np.cos(df['zenith'])
     df['log_energy_loss'] = np.log10(df['energy_loss'])
     df['log_m_r'] = np.log10(df['m_r'])
     df['log_s_r'] = np.log10(df['s_r'])
     df['log_charge'] = np.log10(df['charge'])
     df['log_A'] = np.log10(df['A'])
-    m_o = df['m_o'].values
-    m_125 = df['m_125'].values
-    df['new_s125'] = [i + 125*j for i,j in zip(df['s_o'],df['s_r'])]
-    df = df.loc[
-                (df['S125']>0)&
-                (df['fit_status_m']!=0)&
-                (df['fit_status_m']!=2)&
-                (df['m_chi2']>1e-4)&
-                (df['log_m_r']>-5)&
-                (df['log_m_r']<-1)&
-                (df['m_125']<6)&
-                (df['A']>1e-4)&
-               (df['zenith']<45*(np.pi/180))]
-    return df
+    df['new_s125'] = df['s_o'] + 125 * df['s_r']
 
-def binning(x,y,bins,bins_input=[]):
-    check1 = list(zip(x,y))
-    if len(bins_input)!=0:
-        hist1 = np.histogram(x,bins=bins_input)
-    else:
-        hist1 = np.histogram(x,bins=bins)
-    mean = [[] for i in range(len(hist1[1][0:len(hist1[1])-1]))]
-    mean_count = np.zeros(len(mean))
-    index = range(len(mean))
-    for i in check1:
-        for j in index:
-            if (i[0] > hist1[1][j] and i[0] < hist1[1][j+1]):
-                mean[j].append( i[1])
-                mean_count[j]+=1
+def apply_data_filters(df):
+    return df.loc[
+        (df['S125'] > 0) & (df['fit_status_m'].isin([0, 2])) &
+        (df['m_chi2'] > 1e-4) & (-5 < df['log_m_r'] < -1) &
+        (df['m_125'] < 6) & (df['A'] > 1e-4) &
+        (df['zenith'] < 45 * (np.pi / 180))]
 
-    new_dist_10 = []
-    new_dist_90 =  []
-    check = []
-    for j in mean:
-        if len(j) == 0:
-            check.append(False)
-            continue
-        else:
-            check.append(True)
-        dist10 = []
-        dist90 = []
-        for k in range(100):
-            new_sample = resample(j)
-            dist10.append(np.quantile(new_sample,0.25))
-            dist90.append(np.quantile(new_sample,0.75))
-        new_dist_10.append(dist10)
-        new_dist_90.append(dist90)
-    mean_overall = np.array([np.mean(i) for i in mean])[check]
-    std_mean = np.array([np.std(i) for i in mean])[check]
-    median = np.array([np.median(i) for i in mean])[check]
-    ten = [np.mean(i) for i in new_dist_10]
-    ninety = [np.mean(i) for i in new_dist_90]
+def binning(x, y, bins, bins_input=None):
+    bins_input = bins_input or bins
+    hist1, bin_edges = np.histogram(x, bins=bins_input)
+    indices = np.digitize(x, bin_edges[:-1])
     
-    bins = np.array(hist1[1][0:len(hist1[1])-1])[check]
-
-    return mean_overall,std_mean,bins,median,ten,ninety
-
-def binning2(x,y,bins,confidence_interval = 1,bins_input=[]):
-    check1 = list(zip(x,y))
-    if len(bins_input)!=0:
-        hist1 = np.histogram(x,bins=bins_input)
-    else:
-        hist1 = np.histogram(x,bins=bins)
-    mean = [[] for i in range(len(hist1[1][0:len(hist1[1])-1]))]
-    mean_count = np.zeros(len(mean))
-    index = range(len(mean))
-    for i in check1:
-        for j in index:
-            if (i[0] > hist1[1][j] and i[0] < hist1[1][j+1]):
-                mean[j].append( i[1])
-                mean_count[j]+=1
-
-    new_dist_10 = []
-    new_dist_90 =  []
-    check = []
-    for j in mean:
-        if len(j) == 0:
-            check.append(False)
-            continue
-        else:
-            check.append(True)
-        dist10 = []
-        dist90 = []
-        for k in range(100):
-            new_sample = resample(j)
-            if confidence_interval == 1:
-                dist10.append(np.quantile(new_sample,0.25))
-                dist90.append(np.quantile(new_sample,0.75))
-            elif confidence_interval == 2:
-                dist10.append(np.quantile(new_sample,0.10))
-                dist90.append(np.quantile(new_sample,0.90))
-        new_dist_10.append(dist10)
-        new_dist_90.append(dist90)
-    mean_overall = np.array([np.mean(i) for i in mean])[check]
-    std_mean = np.array([np.std(i) for i in mean])[check]
-    median = np.array([np.median(i) for i in mean])[check]
-    ten = [np.mean(i) for i in new_dist_10]
-    ninety = [np.mean(i) for i in new_dist_90]
-    FWHM = np.array([2*(2*np.log(2))**0.5 * np.std(i) for i in mean])[check]
+    mean = [y[indices == i].tolist() for i in range(1, len(bin_edges))]
     
-    bins = np.array(hist1[1][0:len(hist1[1])-1])[check]
+    new_dist_10 = [np.quantile(resample(j), 0.25) for j in mean]
+    new_dist_90 = [np.quantile(resample(j), 0.75) for j in mean]
+    
+    mean_overall = np.array([np.mean(i) for i in mean if i]).tolist()
+    std_mean = np.array([np.std(i) for i in mean if i]).tolist()
+    median = np.array([np.median(i) for i in mean if i]).tolist()
+    ten = np.array([np.mean(i) for i in new_dist_10])
+    ninety = np.array([np.mean(i) for i in new_dist_90])
+    
+    return mean_overall, std_mean, bin_edges[:-1], median, ten, ninety
 
-    return mean,mean_overall,std_mean,bins,median,ten,ninety,FWHM
+def binning2(x, y, bins, confidence_interval=1, bins_input=None):
+    bins_input = bins_input or bins
+    hist1, bin_edges = np.histogram(x, bins=bins_input)
+    indices = np.digitize(x, bin_edges[:-1])
+    
+    mean = [y[indices == i].tolist() for i in range(1, len(bin_edges))]
+    
+    new_dist_10 = [np.quantile(resample(j), 0.10) for j in mean]
+    new_dist_90 = [np.quantile(resample(j), 0.90) for j in mean]
+    
+    mean_overall = np.array([np.mean(i) for i in mean if i]).tolist()
+    std_mean = np.array([np.std(i) for i in mean if i]).tolist()
+    median = np.array([np.median(i) for i in mean if i]).tolist()
+    ten = np.array([np.mean(i) for i in new_dist_10])
+    ninety = np.array([np.mean(i) for i in new_dist_90])
+    FWHM = np.array([2 * (2 * np.log(2))**0.5 * np.std(i) for i in mean if i]).tolist()
+    
+    return mean, mean_overall, std_mean, bin_edges[:-1], median, ten, ninety, FWHM
 
 def plot_function(true,predicted,min_value,max_value,bins_hist,bins,name):
     bins_input = np.linspace(min_value,max_value,bins+1)
@@ -227,13 +170,11 @@ def plot_function(true,predicted,min_value,max_value,bins_hist,bins,name):
     plt.tight_layout()
     plt.savefig(name+'2.png')
     
-def cut_values(energy,mass_value = None,mass=[]):
-    cut_energy = np.linspace(6.5,8,11)
+def cut_values(energy, mass_value=None, mass=[]):
+    cut_energy = np.linspace(6.5, 8, 11)
     count = []
-    for i in range(len(cut_energy[0:-1])):
-        if (len(mass)==0)&(mass_value == None):
-            count.append(np.sum((energy>cut_energy[i])&(energy<cut_energy[i+1])))
-        else:
-            count.append(np.sum((energy>cut_energy[i])&(energy<cut_energy[i+1])&(mass==mass_value)))
+    for i in range(len(cut_energy) - 1):
+        mass_condition = (len(mass) == 0) & (mass_value is None) or (mass == mass_value)
+        count.append(np.sum((energy > cut_energy[i]) & (energy < cut_energy[i + 1]) & mass_condition))
     return count
 
